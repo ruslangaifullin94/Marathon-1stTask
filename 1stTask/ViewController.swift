0125cf3812c71aaf417fc1fb5cde443781a5220c
Ruslan Gaifullin <gayfullin.r.i@yandex.ru>
2023-05-01 15:17:16 +0300

    Initial Commit

---
+++ 1stTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1stTask
+//
+//  Created by Руслан Гайфуллин on 01.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
